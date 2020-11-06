1b5148a88ce2151e0059e5a9be5e3b16a6b43240
atsutomori-82 <atsutomori.82@gmail.com>
2020-11-06 18:01:56 +0900

    issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,13 +8,13 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
+    Plan.create(plan)
     redirect_to action: :index
   end
 
   private
 
-  def plan_params
+  def plan
     params.require(:calendars).permit(:date, :plan)
   end
 
